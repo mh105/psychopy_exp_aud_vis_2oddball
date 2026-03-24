035b9b656c9899c81c43ed0fbba6b8e020ea1beb
mh105 <alexhe0218@gmail.com>
2026-03-24 14:28:40 -0700

    ENH: add 200ms sound effect stimuli

---
+++ resource/stimuli_200ms/categories.py
@@ -0,0 +1,10 @@
+CATEGORIES = [
+    "dog", "rooster", "pig", "cow", "frog",
+    "cat", "hen", "insects", "sheep", "crow",
+    "laughing", "sneezing", "coughing", "clapping", "footsteps",
+    "door_wood_knock", "mouse_click", "keyboard_typing", "door_wood_creaks", "can_opening",
+    "washing_machine", "vacuum_cleaner", "clock_alarm", "glass_breaking", "drinking_sipping",
+    "crickets", "chirping_birds", "wind", "water_drops", "pouring_water",
+    "engine", "train", "church_bells", "fireworks", "car_horn",
+    "siren", "chainsaw", "hand_saw", "toilet_flush", "brushing_teeth",
+]
