c0aa902b08f1df3af4630ec63b37f33b3142b0d3
mh105 <alexhe0218@gmail.com>
2026-03-25 09:33:05 -0700

    BF: change .wav files to 48000Hz

--- resource/stimuli_200ms/categories.py
+++ resource/stimuli_200ms/categories.py
@@ -1,10 +1,12 @@
 CATEGORIES = [
-    "dog", "rooster", "pig", "cow", "frog",
-    "cat", "hen", "insects", "sheep", "crow",
-    "laughing", "sneezing", "coughing", "clapping", "footsteps",
+    "dog", "pig", "cow", "frog",
+    "cat", "insects", "sheep",
+    "laughing", "sneezing", "breathing", "crying_baby", "clapping", "footsteps",
     "door_wood_knock", "mouse_click", "keyboard_typing", "door_wood_creaks", "can_opening",
-    "washing_machine", "vacuum_cleaner", "clock_alarm", "glass_breaking", "drinking_sipping",
+    "washing_machine", "vacuum_cleaner", "clock_alarm", "drinking_sipping",
     "crickets", "chirping_birds", "wind", "water_drops", "pouring_water",
+    "rain", "crackling_fire", "helicopter",
     "engine", "train", "church_bells", "fireworks", "car_horn",
     "siren", "chainsaw", "hand_saw", "toilet_flush", "brushing_teeth",
+    "440Hz", "880Hz", "493.88Hz", "1100Hz",
 ]
